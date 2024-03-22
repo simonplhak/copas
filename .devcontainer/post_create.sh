e292d128dc571b2602a724df89e7cf0422b4634d
Simon Plhak <plhak.s@gmail.com>
2024-03-22 09:36:30 +0100

    fix: creating new devcontainer

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -1,6 +1,6 @@
-mkdir ~/.config/tmuxinator -p && cp cp /copas-docker/.devcontainer/copas.yml ~/.config/tmuxinator/copas.yml
+mkdir ~/.config/tmuxinator -p && cp /copas-docker/.devcontainer/copas.yml ~/.config/tmuxinator/copas.yml
 apt update
-apt install -y software-properties-common
+apt install -y software-properties-common git
 add-apt-repository ppa:deadsnakes/ppa -y
 apt install -y python3.10 python3-pip inetutils-ping git python3.10-venv curl vim netcat tmuxinator mariadb-server mariadb-client
 cd /copas-docker/backend && python3.10 -m venv docker_env && source docker_env/bin/activate && pip install poetry==1.3.2 && poetry install && deactivate
@@ -8,4 +8,4 @@ cd /copas-docker/backend && python3.10 -m venv docker_env && source docker_env/b
 mkdir -p /run/mysqld
 git clone https://github.com/CTFd/CTFd.git /ctfd
 cd /ctfd && python3.10 -m venv env && source env/bin/activate && pip install -r requirements.txt && deactivate
-sh /copas-docker/game_utils/ctfd.entrypoint.sh
+sh /copas-docker/game_utils/ctfd/ctfd.entrypoint.sh
