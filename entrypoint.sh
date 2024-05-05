fbd31b050b9cea60cd3deb0d89e10ea23a3c0955
Simon Plhak <plhak.s@gmail.com>
2024-05-05 16:08:16 +0200

    fix: create secrets only if they do not already exist

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
-openssl rand -base64 24 >/etc/secret_token.txt && export SECRET_KEY=$(cat /etc/secret_token.txt)
-openssl rand -base64 24 >/etc/password.txt && export PASSWORD=$(cat /etc/password.txt)
+if [ ! -f /etc/secret_token.txt ]; then
+  openssl rand -base64 24 >/etc/secret_token.txt && export SECRET_KEY=$(cat /etc/secret_token.txt)
+fi
+if [ ! -f /etc/password.txt ]; then
+  openssl rand -base64 24 >/etc/password.txt && export PASSWORD=$(cat /etc/password.txt)
+fi
 exec supervisord -c /etc/supervisord.conf
