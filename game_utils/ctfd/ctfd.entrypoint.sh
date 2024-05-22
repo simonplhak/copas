1517745aba2d6b2cd81cd66f0a0c719910216447
Simon Plhak <plhak.s@gmail.com>
2024-05-22 19:16:38 +0200

    fix: handle ctfd port env variable not specified

--- game_utils/ctfd/ctfd.entrypoint.sh
+++ game_utils/ctfd/ctfd.entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 /ctfd/env/bin/flask db upgrade
 
-exec /ctfd/env/bin/gunicorn wsgi:app --bind "0.0.0.0:$CTFD_PORT" --chdir /ctfd --workers 4
+exec /ctfd/env/bin/gunicorn wsgi:app --bind "0.0.0.0:${CTFD_PORT:-8001}" --chdir /ctfd --workers 4
